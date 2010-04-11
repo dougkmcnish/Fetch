fc6dc2284c71f455cc0624c7eadbe74a0ef9190b
Doug McNish <git@stopavoiding.us>
2010-04-11 18:18:54 -0400

    documentation cleanup

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-## fetch.rb ##
+fetch.rb
+=========
 
-A simple fetchmail clone.  
+POPs mail off of a POP3 server 
+and stores the mail in a Maildir
+of your choosing.  The POP3 portion
+is almost a straight copy/paste 
+from the Net::POP3 docs, with a couple
+changes to fake Maildir support.
+The rest is a novice coder reinventing
+the wheel. ;)
+
+
+FIXME
+-------
+Should probably do a 
+better job of creating our 
+Maildir structure. Until then you\'ll
+need to make sure to create 
+$inbox/(new|cur|tmp)
+
+You\'ll currently need to manually
+configure host/user/pass in-script
+as well.  
--- fetch.rb
+++ fetch.rb
@@ -1,27 +1,36 @@
 #!/usr/bin/env ruby
-=begin
+
+HELPMSG=<<-EOS
 
 fetch.rb: 
   POPs mail off of a POP3 server 
   and stores the mail in a Maildir
   of your choosing.  The POP3 portion
   is almost a straight copy/paste 
-  from the Net::POP3 docs.  The rest
-  is a novice coder reinventing
+  from the Net::POP3 docs, with a couple
+  changes to fake Maildir support.
+  The rest is a novice coder reinventing
   the wheel. ;)
   
   FIXME: Should probably do a 
   better job of creating our 
-  Maildir structure. Until then
-  Just make sure to create 
-  #{INBOX}/(new|cur|tmp)
+  Maildir structure. Until then you\'ll
+  need to make sure to create 
+  $inbox/(new|cur|tmp)
   
+  You\'ll currently need to manually
+  configure host/user/pass in-script
+  as well. 
   
-=end 
+EOS
 
 require 'socket'
 require 'net/pop'
 
+if ARGV.include? '-h' or ARGV.include? '--help' or ARGV.include? '-?'
+  puts "#{HELPMSG}"
+  exit
+end
 
 $server = 'changeme'
 $user = 'changeme'
